9e6059b41fd6d12c4aa31ca07909233d7c57c41e
emreagann <159646253+emreagann@users.noreply.github.com>
2025-06-02 20:16:35 +0300

    Update t2.py

--- t2.py
+++ t2.py
@@ -8,11 +8,11 @@ class T2NeutrosophicNumber:
         return f"T2N(truth={self.truth:.3f}, indeterminacy={self.indeterminacy:.3f}, falsity={self.falsity:.3f})"
 
     def __add__(self, other):
-      return T2NeutrosophicNumber(
+     return T2NeutrosophicNumber(
         self.truth + other.truth,
         self.indeterminacy + other.indeterminacy,
         self.falsity + other.falsity
-      )
+     )
 
 
     def __mul__(self, other):
@@ -23,7 +23,13 @@ class T2NeutrosophicNumber:
         )
 
     def score(self):
-        return self.truth - self.falsity
+  
+     val = (8 + (self.truth**2 + 2*self.truth*self.indeterminacy + self.truth*self.falsity)
+             - (self.indeterminacy*self.truth + 2*self.indeterminacy**2 + self.indeterminacy*self.falsity)
+             - (self.falsity*self.truth + 2*self.falsity*self.indeterminacy + self.falsity**2))
+
+     return val / 12
+
 
 def classic_to_t2n(value, indeterminacy=0.1):
 
