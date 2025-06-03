374246d59fc90e3f1e2a20054ca856052b2d8e81
emreagann <159646253+emreagann@users.noreply.github.com>
2025-06-03 16:00:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import re
 from t2 import T2NeutrosophicNumber, classic_to_t2n
 
 st.title("T2 Neutrosophic MABAC for Decision Making")
