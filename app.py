99412e9ba6ca2dee60ba1398632fe339376a4ffd
emreagann <159646253+emreagann@users.noreply.github.com>
2025-06-09 17:29:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def convert_range_to_t2n(a, b):
         m = (a + b) / 2
         T = (a/10, m/10, b/10)
         I = (0.0125, 0.0125, 0.0125)
-        F = (1 - b/10, 1 - ((a + b)/2)/10, 1 - a/10) if a != b else (1 - a/10, 1 - a/10, 1 - a/10)
+        F = (1 - b/10, 1 - m/10, 1 - a/10)
         return T2NN(T, I, F)
 
 def normalize_minmax(values, benefit=True):
