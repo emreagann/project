5a80420b9d77802182518b463e131923bdb53b8b
emreagann <159646253+emreagann@users.noreply.github.com>
2025-06-10 15:23:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ if uploaded_file is not None:
     # Linguistic Variables sayfalarındaki her sayfadan verileri alalım
     linguistic_values_alternatives = {}
     for index, row in df.iterrows():
-        linguistic_values_alternatives[row['Alternatives']] = row[['αα', 'αβ', 'αY', 'βα', 'ββ', 'βY', 'γα', 'γβ', 'YY']].values.tolist()
+        linguistic_values_alternatives[row['Alternatives']] = row[['αα', 'αβ', 'αY', 'βα', 'ββ', 'βY', 'γα', 'γβ', 'γγ']].values.tolist()
 
     linguistic_values_weights = {}
     for index, row in weights_df.iterrows():
-        linguistic_values_weights[row['Weights']] = row[['αα', 'αβ', 'αY', 'βα', 'ββ', 'βY', 'γα', 'γβ', 'YY']].values.tolist()
+        linguistic_values_weights[row['Weights']] = row[['αα', 'αβ', 'αY', 'βα', 'ββ', 'βY', 'γα', 'γβ', 'γγ']].values.tolist()
 
     # Linguistik terimleri sayılara dönüştürme fonksiyonu
     def convert_linguistic_to_score(value, linguistic_values):
